dad3160f91e4055d33c94b60a8bbf1113e821518
Onder Cataltepe <128795481+cataltepeonder@users.noreply.github.com>
2026-01-04 13:01:51 +0300

    Update faq with real questions

--- src/screens/home/components/Faq/Faq.tsx
+++ src/screens/home/components/Faq/Faq.tsx
@@ -7,7 +7,7 @@ const Faq = () => {
       <Container>
         <div flex="~ col items-center" gap="6" mb="sm:10 8" text="center">
           <h2 line-height="tight" text="center lg:3rem 2rem gray-dark ">
-            Do you have any Question
+            Do you have any question?
           </h2>
         </div>
 
