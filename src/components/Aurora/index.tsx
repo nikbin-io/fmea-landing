fc4ef6480168bfa38fe4d2264568ffba87d88c82
Onder Cataltepe <128795481+cataltepeonder@users.noreply.github.com>
2026-02-04 23:43:00 +0300

    Try to fix aurora for android

--- src/components/Aurora/index.tsx
+++ src/components/Aurora/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 
 const VERT = `
-in vec3 position;
+attribute vec3 position;
 void main() {
   gl_Position = vec4(position.xy, 0.0, 1.0);
 }
@@ -17,14 +17,12 @@ uniform vec3 uColorStops[3];
 uniform vec2 uResolution;
 uniform float uBlend;
 
-out vec4 fragColor;
-
 vec3 permute(vec3 x) {
   return mod(((x * 34.0) + 1.0) * x, 289.0);
 }
 
 float snoise(vec2 v){
-  const vec4 C = vec4(
+  vec4 C = vec4(
       0.211324865405187, 0.366025403784439,
       -0.577350269189626, 0.024390243902439
   );
@@ -63,35 +61,18 @@ float snoise(vec2 v){
   return 130.0 * dot(m, g);
 }
 
-struct ColorStop {
-  vec3 color;
-  float position;
-};
-
-#define COLOR_RAMP(colors, factor, finalColor) {              \
-  int index = 0;                                            \
-  for (int i = 0; i < 2; i++) {                               \
-     ColorStop currentColor = colors[i];                    \
-     bool isInBetween = currentColor.position <= factor;    \
-     index = int(mix(float(index), float(i), float(isInBetween))); \
-  }                                                         \
-  ColorStop currentColor = colors[index];                   \
-  ColorStop nextColor = colors[index + 1];                  \
-  float range = nextColor.position - currentColor.position; \
-  float lerpFactor = (factor - currentColor.position) / range; \
-  finalColor = mix(currentColor.color, nextColor.color, lerpFactor); \
+vec3 colorRamp(vec3 color0, vec3 color1, vec3 color2, float factor) {
+  if (factor < 0.5) {
+    return mix(color0, color1, factor * 2.0);
+  } else {
+    return mix(color1, color2, (factor - 0.5) * 2.0);
+  }
 }
 
 void main() {
   vec2 uv = gl_FragCoord.xy / uResolution;
 
-  ColorStop colors[3];
-  colors[0] = ColorStop(uColorStops[0], 0.0);
-  colors[1] = ColorStop(uColorStops[1], 0.5);
-  colors[2] = ColorStop(uColorStops[2], 1.0);
-
-  vec3 rampColor;
-  COLOR_RAMP(colors, uv.x, rampColor);
+  vec3 rampColor = colorRamp(uColorStops[0], uColorStops[1], uColorStops[2], uv.x);
 
   float height = snoise(vec2(uv.x * 2.0 + uTime * 0.1, uTime * 0.25)) * 0.5 * uAmplitude;
   height = exp(height);
@@ -103,7 +84,7 @@ void main() {
 
   vec3 auroraColor = intensity * rampColor;
 
-  fragColor = vec4(auroraColor * auroraAlpha, auroraAlpha);
+  gl_FragColor = vec4(auroraColor * auroraAlpha, auroraAlpha);
 }
 `
 
@@ -206,7 +187,6 @@ const Aurora = (props: AuroraProps) => {
       vertexShader: VERT,
       fragmentShader: FRAG,
       uniforms,
-      glslVersion: THREE.GLSL3,
       transparent: true,
       premultipliedAlpha: true,
       depthTest: false,
